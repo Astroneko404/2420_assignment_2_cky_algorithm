eff9a70f83e22e892c2464853d42f885740b4bad
Astroneko404 <astroneko404@outlook.com>
2019-10-05 23:05:24 -0400

    CNF update [10/05/2019]

--- CKY.py
+++ CKY.py
@@ -1,6 +1,10 @@
 from decimal import Decimal
 from collections import defaultdict
+from Node import Node
 import re
+
+
+# terminal = ['Det', 'Noun', 'Verb', 'Pronoun']
 
 
 def print_nested_dict(d):
@@ -17,6 +21,12 @@ def print_nested_dict(d):
     return
 
 
+def print_table(t):
+    for i in range(len(t)):
+        print(t[i])
+    return
+
+
 class CKY:
     def __init__(self, grammar_txt):
         self.grammar_txt = grammar_txt
@@ -28,6 +38,7 @@ class CKY:
         self.build_pcfg()
         self.binarize()
         self.check_prob()
+        # print_nested_dict(self.grammar)
 
     def build_pcfg(self):
         """
@@ -49,7 +60,7 @@ class CKY:
                         child = tuple(i for i in child.split())
                         self.grammar[parent][child] = Decimal(prob)
                     else:  # Lexicon part
-                        self.lexicon[parent][child] = Decimal(prob)
+                        self.lexicon[parent][child.lower()] = Decimal(prob)
             # print_nested_dict(self.grammar)
             # print_nested_dict(self.lexicon)
         file.close()
@@ -78,6 +89,18 @@ class CKY:
                         grammar_tmp[parent][tuple([label]) + rest] = grammar_tmp[parent].pop(child)
 
         self.grammar = grammar_tmp
+
+        # Convert unit productions
+        for parent in list(self.grammar):
+            for child in list(self.grammar[parent]):
+                if len(child) == 1 and child[0] not in self.lexicon:
+                    base_prob = self.grammar[parent][child]
+                    self.grammar[parent].pop(child)
+                    # print(child[0], base_prob)
+                    for rule, prob in self.grammar[child[0]].items():
+                        # print(rule, prob)
+                        self.grammar[parent][rule] = prob * base_prob
+
         print_nested_dict(self.grammar)
 
     def check_prob(self):
@@ -85,3 +108,50 @@ class CKY:
             assert sum([self.grammar[parent][i] for i in self.grammar[parent]]) == Decimal(1.0)
         for parent in self.lexicon:
             assert sum([self.lexicon[parent][i] for i in self.lexicon[parent]]) == Decimal(1.0)
+
+    def build_tree(self, back, table, n):
+        print(back[1][n])
+        return
+
+    def prob_cky(self, words):
+        n = len(words)
+        table = [[dict() for i in range(n+1)] for j in range(n+1)]
+        back = [[dict() for i in range(n+1)] for j in range(n+1)]
+
+        for j in range(1, n+1):
+            for A in self.lexicon:
+                if words[j-1] in self.lexicon[A]:
+                    table[j-1][j][A] = (self.lexicon[A][words[j-1]])
+            for A in self.grammar:
+                for rule, prob in self.grammar[A].items():
+                    if len(rule) == 1:
+                        B = rule[0]
+                        print(A, B)
+                        if B in table[j-1][j]:
+                            table[j-1][j][A] = table[j-1][j][B] * prob
+
+            # print(table[0][1])
+
+            for i in reversed(range(0, j - 1)):
+                for k in range(i+1, j):
+                    for A in self.grammar:
+                        # print(self.grammar[A])
+                        for rule, prob in self.grammar[A].items():
+                            if len(rule) == 2:
+                                B, C = rule[0], rule[1]
+                                print(B, table[i][k])
+                                print(C, table[k][j])
+                                print()
+                                if B in table[i][k] and C in table[k][j]:
+                                    # print(i, j, k)
+                                    # print(B, C)
+                                    # print(prob * table[i][k][B] * table[k][j][C])
+                                    if table[i][j][A] < prob * table[i][k][B] * table[k][j][C]:
+                                        table[i][j][A] = prob * table[i][k][B] * table[k][j][C]
+                                        back[i][j][A] = (k, B, C)
+
+        # self.build_tree(back, table, n)
+        # for i in range(len(table)):
+        #     for j in range(len(table[0])):
+        #         print(i, j, table[i][j])
+        return
---
+++ Node.py
@@ -0,0 +1,10 @@
+class Node:
+
+    def __init__(self, root, left, right, end):
+        self.root = root
+        self.left = left
+        self.right = right
+        self.terminal = end
+        # self._status = True
+        # if end == None:
+        #     self._status = False
